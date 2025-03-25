b32c38f9535141330bd0e0cf4030ffc8edb9199d
arlindaislami <arlindaaislamii@gmail.com>
2025-03-25 14:55:29 +0100

    Implementation of tab bar view and file creation

---
+++ app/src/main/java/com/example/mycvandroid/NewFile.kt
@@ -0,0 +1,21 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun NewFileScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize().padding(20.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "New File", fontSize = 24.sp)
+    }
+}
