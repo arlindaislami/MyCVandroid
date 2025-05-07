29173362e4efca1d4aa6c39c21b6f515011cd4dc
arlindaislami <arlindaaislamii@gmail.com>
2025-05-07 13:30:53 +0200

    fix problem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,10 +10,10 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        applicationId = "com.arlindai.mycvandroid"
+        applicationId = "com.ai.mycvandroid"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
+        versionCode = 3
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
